b0520acd6213833c9a00181fc5d56e61fb8c9691
poooootaaaaaa-boop <poooootaaaaaa@gmail.com>
2026-06-30 15:19:15 +0000

    mejoras de los seders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,46 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+         $this->call([
+                       RolesSeeder::class,
+            EspecialidadesSeeder::class,
+            UsuariosSeeder::class,
+            ClinicasSeeder::class,
+            FarmaciasSeeder::class,
+            PlanesSeeder::class,
+            TiposPagoDoctorSeeder::class,
+            CategoriasMedicamentosSeeder::class,
+            DistribuidoresSeeder::class,
+            InstrumentosMedicosSeeder::class,
+            MedicamentosSeeder::class,
+            ConsultoriosSeeder::class,
+            HabitacionesSeeder::class,
+            DoctoresSeeder::class,
+            DoctorConsultoriosSeeder::class,
+            HorariosDoctoresSeeder::class,
+            PacientesSeeder::class,
+            InventarioSeeder::class,
+            ConsultorioInstrumentosSeeder::class,
+            CitasSeeder::class,
+            ConsultasSeeder::class,
+            RecetasSeeder::class,
+            RecetaDetalleSeeder::class,
+            BloqueosHorariosSeeder::class,
+            OcupacionHabitacionesSeeder::class,
+            SuscripcionesSeeder::class,
+            HistorialPagosSeeder::class,
+            HistorialSuscripcionesSeeder::class,
+            PagosDoctoresSeeder::class,
+            PagosDoctoresHistorialSeeder::class,
+            NotificacionesSeeder::class,
+            UsuarioNotificacionesSeeder::class,
+            PreferenciasNotificacionesSeeder::class,
+            ExpedienteArchivosSeeder::class,
+            IALogsSeeder::class,
+            MedicamentosCaducadosSeeder::class,
+            MovimientosInventarioSeeder::class,
+            UsersSeeder::class,
+
         ]);
     }
 }
